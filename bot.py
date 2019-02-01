8052b8a254d54d824ae305e32f793ddf20eda0a0
MeHdi <mehdi.kazemi8@gmail.com>
2019-02-01 14:47:01 +0330

    show user options in cat1, cat2 and cat3, and show the number of results to be shown

--- bot.py
+++ bot.py
@@ -1,21 +1,11 @@
-import logging
 from enum import Enum
 
 import pandas as pd
 from telegram.ext import CommandHandler, MessageHandler, Filters
 from telegram.ext import Updater
 
-updater = Updater(token='783418650:AAGAuqiOzVC3FMFsous2Hs_a9DU8AUdy_rQ')
-dispatcher = updater.dispatcher
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-
-print("Please wait, preparing the data...\n")
-df = pd.read_csv('divar.csv')
-print("Data is ready!\n")
-
 
 class CurrentState(Enum):
-    MAIN_MENU = 0
     CAT1 = 1
     CAT2 = 2
     CAT3 = 3
@@ -27,6 +17,9 @@ def get_all_options(df, column):
 
 
 def make_str_from(options):
+    options = [opt for opt in options if opt is not None and not pd.isna(opt)]
+
+    print("make_str_from", options)
     message = ""
     ii = 0
     for option in options:
@@ -68,18 +61,84 @@ while True:
 """
 
 
+def do_log(update):
+    logging = open("log_file.txt", "a")
+    logging.write(str(update) + "\n")
+    logging.close()
+
+
 def start(bot, update):
-    logging.log(level=0, msg=str(update))
-    print(update)
-    message = "Which category do you want to search?\n" + make_str_from(get_all_options(df, 'cat1'))
+    global current_state, cat1, conditions
+
+    do_log(update)
+    conditions = []
+    cat1 = get_all_options(df, 'cat1')
+    message = "Which category do you want to search?\n" + make_str_from(cat1)
     bot.send_message(chat_id=update.message.chat_id, text=message)
+
+    current_state = CurrentState.CAT1
+
+
+def prepare_conditions(df, conditions):
+    if len(conditions) == 0:
+        return df['id'] > 0
+
+    result = df['id'] > 0
+    for column, value in conditions:
+        result = result & (df[column] == value)
+    return result
 
 
 def handle_text(bot, update):
-    pass
+    global current_state, cat1, cat2, cat3, conditions
+
+    print("CurrentState ->", current_state)
+    print("Conditions ->", conditions)
+
+    if current_state == CurrentState.CAT1:
+        idx = int(update.message.text)
+        conditions.append(('cat1', cat1[idx]))
+        cat2 = list(df[prepare_conditions(df, conditions)]['cat2'].unique())
+        message = "Choose one?\n" + make_str_from(cat2)
+        bot.send_message(chat_id=update.message.chat_id, text=message)
+
+        current_state = CurrentState.CAT2
+
+    elif current_state == CurrentState.CAT2:
+        idx = int(update.message.text)
+        conditions.append(('cat2', cat2[idx]))
+        cat3 = list(df[prepare_conditions(df, conditions)]['cat3'].unique())
+        message = "Choose one?\n" + make_str_from(cat3)
+        bot.send_message(chat_id=update.message.chat_id, text=message)
+
+        current_state = CurrentState.CAT3
+
+    elif current_state == CurrentState.CAT3:
+
+        idx = int(update.message.text)
+        conditions.append(('cat3', cat3[idx]))
+        message = "There are {} options".format(len(df[prepare_conditions(df, conditions)]))
+        bot.send_message(chat_id=update.message.chat_id, text=message)
+
+        print("Conditions ->", conditions)
+
+    print("CurrentState ->", current_state)
+    print("Conditions ->", conditions)
 
 
-current_state = CurrentState(CurrentState.MAIN_MENU)
+updater = Updater(token='783418650:AAGAuqiOzVC3FMFsous2Hs_a9DU8AUdy_rQ')
+dispatcher = updater.dispatcher
+
+print("Please wait, preparing the data...\n")
+df = pd.read_csv('divar.csv')
+print(df.columns)
+print("Data is ready!\n")
+
+current_state = CurrentState(CurrentState.CAT1)
+cat1 = []
+cat2 = []
+cat3 = []
+conditions = []
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
